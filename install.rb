b332cfa2435c739c0bcfdbf50ef541f2a00a6374
studioda <studioda@7491b73d-821b-0410-9297-ad1f6b5b4194>
2007-02-21 16:58:15 +0000

    included engines install warning
    git-svn-id: http://svn.6brand.com/projects/plugins/simple_pages@308 7491b73d-821b-0410-9297-ad1f6b5b4194

--- install.rb
+++ install.rb
@@ -5,3 +5,12 @@ puts "If you want to use version-control with your pages you'll need to run the 
 puts "Just copy and past the following into your shell:"
 puts ''
 puts "    ruby #{RAILS_ROOT}/script/plugin install -x http://svn.techno-weenie.net/projects/plugins/acts_as_versioned"
+puts ''
+
+unless File.exists?("#{RAILS_ROOT}/vendor/plugins/engines")
+  puts "WARNING: you do not have the engines plugin installed.  This is a necessary requirement for the simple_pages plugin to work."
+  puts "Please install the engines plugin by running the following command in your shell:"
+  puts ''
+  puts "    ruby #{RAILS_ROOT}/script/plugin install -x http://svn.rails-engines.org/plugins/engines"
+  puts ''
+end
