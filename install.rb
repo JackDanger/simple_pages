793617937ab79d54b2f7cae14015b14347874e99
studioda <studioda@7491b73d-821b-0410-9297-ad1f6b5b4194>
2007-02-21 16:34:24 +0000

    final try?
    git-svn-id: http://svn.6brand.com/projects/plugins/simple_pages@302 7491b73d-821b-0410-9297-ad1f6b5b4194

--- install.rb
+++ install.rb
@@ -1,7 +1,3 @@
 puts IO.read(File.join(File.dirname(__FILE__), 'README'))
 
-p Kernel.gets
-
-puts "Do you want to use version-control with your pages? (Recommended)"
-puts "[y/n]:"
-system("ruby #{RAILS_ROOT}/script/plugin install -x http://svn.techno-weenie.net/projects/plugins/acts_as_versioned") unless gets.chomp.downcase == 'n'
+load File.join(File.dirname(__FILE__), 'install-process.rb')
