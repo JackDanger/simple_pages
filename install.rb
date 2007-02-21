7aa358316e89caca389ca681a4daf6959294a624
studioda <studioda@7491b73d-821b-0410-9297-ad1f6b5b4194>
2007-02-21 01:33:39 +0000

    git-svn-id: http://svn.6brand.com/projects/plugins/simple_pages@292 7491b73d-821b-0410-9297-ad1f6b5b4194

--- install.rb
+++ install.rb
@@ -2,6 +2,8 @@ puts IO.read(File.join(File.dirname(__FILE__), 'README'))
 
 puts "Do you want to use version-control with your pages? (Recommended)"
 puts "[y/n]:"
+x = gets.chomp
+puts x.inspect
 system("ruby #{RAILS_ROOT}/script/plugin install -x http://svn.techno-weenie.net/projects/plugins/acts_as_versioned/") unless gets.chomp.downcase == 'n'
 
 p ARGV
