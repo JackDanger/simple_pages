c0312fcd234ca07b97b185bb649e869eb0f0b619
studioda <studioda@7491b73d-821b-0410-9297-ad1f6b5b4194>
2007-02-21 16:52:33 +0000

    gave up on using gets - it would only use the http pipe.  Folks can now copy and paste the install code straight into their shell.
    git-svn-id: http://svn.6brand.com/projects/plugins/simple_pages@307 7491b73d-821b-0410-9297-ad1f6b5b4194

--- install-process.rb
+++
@@ -1,6 +0,0 @@
-
-puts "Do you want to use version-control with your pages? (Recommended)"
-puts "[y/n]:"
-cmd = "ruby #{RAILS_ROOT}/script/plugin install -x http://svn.techno-weenie.net/projects/plugins/acts_as_versioned"
-puts "cmd: #{cmd}"
-system(cmd) unless Kernel.gets.chomp.downcase == 'n'
--- install.rb
+++ install.rb
@@ -1,7 +1,7 @@
 puts IO.read(File.join(File.dirname(__FILE__), 'README'))
 
-fork do
-  pfile = File.join(File.dirname(__FILE__), 'install-process.rb')
-  puts pfile
-  load pfile
-end
+
+puts "If you want to use version-control with your pages you'll need to run the following command to install Techno-Weenie's ActsAsVersioned plugin (highly recommended)"
+puts "Just copy and past the following into your shell:"
+puts ''
+puts "    ruby #{RAILS_ROOT}/script/plugin install -x http://svn.techno-weenie.net/projects/plugins/acts_as_versioned"
