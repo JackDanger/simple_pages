98c8159cb77430f065794975018f1218a7468042
studioda <studioda@7491b73d-821b-0410-9297-ad1f6b5b4194>
2007-02-21 16:45:47 +0000

    git-svn-id: http://svn.6brand.com/projects/plugins/simple_pages@305 7491b73d-821b-0410-9297-ad1f6b5b4194

--- install.rb
+++ install.rb
@@ -1,3 +1,5 @@
 puts IO.read(File.join(File.dirname(__FILE__), 'README'))
 
-load File.join(File.dirname(__FILE__), 'install-process.rb')
+fork do
+  load File.join(File.dirname(__FILE__), 'install-process.rb')
+end
