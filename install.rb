5d17ab16d4423ad632e68cecf773670b77d912cb
studioda <studioda@7491b73d-821b-0410-9297-ad1f6b5b4194>
2007-02-21 17:01:24 +0000

    tests for engines and aav and provides commands for installation
    git-svn-id: http://svn.6brand.com/projects/plugins/simple_pages@309 7491b73d-821b-0410-9297-ad1f6b5b4194

--- install.rb
+++ install.rb
@@ -1,11 +1,15 @@
 puts IO.read(File.join(File.dirname(__FILE__), 'README'))
 
 
-puts "If you want to use version-control with your pages you'll need to run the following command to install Techno-Weenie's ActsAsVersioned plugin (highly recommended)"
-puts "Just copy and past the following into your shell:"
-puts ''
-puts "    ruby #{RAILS_ROOT}/script/plugin install -x http://svn.techno-weenie.net/projects/plugins/acts_as_versioned"
-puts ''
+unless File.exists?("#{RAILS_ROOT}/vendor/plugins/acts_as_versioned")
+  puts ''
+  puts '--------------------------------------------------'
+  puts "If you want to use version-control with your pages you'll need to run the following command to install Techno-Weenie's ActsAsVersioned plugin (highly recommended)"
+  puts "Just copy and past the following into your shell:"
+  puts ''
+  puts "    ruby #{RAILS_ROOT}/script/plugin install -x http://svn.techno-weenie.net/projects/plugins/acts_as_versioned"
+  puts ''
+end
 
 unless File.exists?("#{RAILS_ROOT}/vendor/plugins/engines")
   puts "WARNING: you do not have the engines plugin installed.  This is a necessary requirement for the simple_pages plugin to work."
