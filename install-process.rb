18c02ad6950e25eb1e3f2f971c8503e865c460de
studioda <studioda@7491b73d-821b-0410-9297-ad1f6b5b4194>
2007-02-21 16:32:43 +0000

    once more - with FEELING
    git-svn-id: http://svn.6brand.com/projects/plugins/simple_pages@300 7491b73d-821b-0410-9297-ad1f6b5b4194

--- install-process.rb
+++
@@ -1,8 +0,0 @@
-puts IO.read(File.join(File.dirname(__FILE__), 'README'))
-
-p __FILE__
-p Kernel.gets
-
-puts "Do you want to use version-control with your pages? (Recommended)"
-puts "[y/n]:"
-#system("ruby #{RAILS_ROOT}/script/plugin install -x http://svn.techno-weenie.net/projects/plugins/acts_as_versioned") unless gets.chomp.downcase == 'n'
--- install.rb
+++ install.rb
@@ -1,5 +1,5 @@
 puts IO.read(File.join(File.dirname(__FILE__), 'README'))
 
-cmd = "ruby #{File.join(File.dirname(__FILE__), 'install-process.rb')}"
-p cmd
-system(cmd)
+puts "Do you want to use version-control with your pages? (Recommended)"
+puts "[y/n]:"
+system("ruby #{RAILS_ROOT}/script/plugin install -x http://svn.techno-weenie.net/projects/plugins/acts_as_versioned") unless gets.chomp.downcase == 'n'
