4533fed5f9f4d2a2c3ee2cab4f6089b54510d402
studioda <studioda@7491b73d-821b-0410-9297-ad1f6b5b4194>
2007-02-22 17:10:13 +0000

    handy new warning
    git-svn-id: http://svn.6brand.com/projects/plugins/simple_pages@315 7491b73d-821b-0410-9297-ad1f6b5b4194

--- app/helpers/simple_pages_helper.rb
+++ app/helpers/simple_pages_helper.rb
@@ -1,2 +1,19 @@
 module SimplePagesHelper
+
+  def simple_new_page_path
+    super
+  rescue
+    raise helper_error
+  end
+  
+  def simple_page_path
+    super
+  rescue
+    raise helper_error
+  end
+
+  def helper_error
+    "You must install this plugin's routes!  Add the line 'map.from_plugin :simple_pages' to your config/routes.rb"
+  end
+
 end
